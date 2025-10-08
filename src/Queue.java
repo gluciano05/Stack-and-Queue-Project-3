4db7bce6f7b9a03e20a1c6dea3ac5b94ef506312
Gabe <llamagabe180@gmail.com>
2025-10-08 01:35:06 -0400

    fully implemented stack and queues

--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,61 @@
-public class Queue {
+//Queue implementation
+public class Queue <E> {
+    private E[] data;
+    private int front;
+    private int rear;
+    private int capacity;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    //I explained the reason for the "SuppressWarnings()" on line 7 of stack
+    public Queue(int givenCapacity) {
+        data = (E[]) new Object[givenCapacity];
+        capacity = givenCapacity;
+        size = 0;
+        front = 0;
+        rear = 0;
+    }
+
+    /** If the queue is not full, adds the item to the rear of the queue
+     * then increments size and rear by 1.
+     * @param item item is the given generic parameter for this function.
+     * @throws RuntimeException if the queue is full
+     */
+    public void enqueue(E item) {
+        if (isFull()) {
+            throw new RuntimeException("Cannot enqueue to a full queue");
+        }
+        data[rear] = item;
+        rear++;
+        size++;
+    }
+
+    /** If the queue is not empty, saves item at the front of the queue
+     * into a temporary variable, increments the front by 1 and decrements size by 1
+     * then, returns the temporary variable.
+     * @return returns the item at the front of the queue
+     * @throws RuntimeException if the queue is empty
+     */
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Cannot dequeue from an empty queue");
+        }
+        E item = data[front];
+        front++;
+        size--;
+        return item;
+    }
+
+    /** Checks if the queue is empty.
+     * @return true if the size of the queue equals 0, else returns false
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    /** Checks if the queue is full.
+     * @return true if the rear equals the capacity - 1, else returns false
+     */
+    public boolean isFull() {
+        return rear == capacity - 1;
+    }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,76 @@
-public class Stack {
+//Stack Implementation
+public class Stack <E> {
+    private E[] data;
+    private int top;
+    private int capacity;
+    private int size;
+
+    @SuppressWarnings("unchecked") //THE COMPILER IS ANNOYING ME WITH THIS WARNING I CANT ANYMORE
+
+    public Stack() {
+
+        /* I am aware I am hard coding but as per the instructions
+         * Stacks are limited to 10 hikers per.
+         * "As soon as a stack becomes full (has 10 hikers in it)"
+         */
+
+        this.data = (E[]) new Object[10];
+        this.top = -1;
+        this.capacity = 10;
+    }
+
+    /** Returns the item at the top of the stack without removing it.
+     * @return the item at the top of the stack
+     */
+    public E peek() {
+        return data[top];
+    }
+
+    /** Removes and returns the item at the top of the stack.
+     * @return the item that was removed from the top of the stack
+     * @throws RuntimeException if the stack is empty
+     */
+    public E pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Cannot pop from empty stack");
+        }
+        E item = data[top];
+        data[top] = null;
+        top--;
+        return item;
+    }
+
+    /** If the stack is not full, increments the top by 1 and sets the tops index equal
+     * to the given data
+     * @param givenData givenData is the data inputted when calling the function
+     * @throws RuntimeException if the stack is full
+     */
+    public void push(E givenData) {
+        if (isFull()) {
+            throw new RuntimeException("Cannot push to a full stack");
+        }
+        top++;
+        data[top] = givenData;
+    }
+
+    /** Checks if the stack is empty.
+     * @return true if the top equals - 1, else returns false
+     */
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    /** Checks if the stack is full.
+     * @return true if the top equals the capacity - 1, else returns false
+     */
+    public boolean isFull() {
+        return top == (capacity - 1);
+    }
+
+    /** A method to get the current size of the stack
+     * @return returns the tops index + 1
+     */
+    public int setSize() {
+        return top + 1;
+    }
 }
